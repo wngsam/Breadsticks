c8a0d3d5af8733ce9a8428a2cb95a7752036b5f6
Sam W <wngsam@outlook.com>
2019-07-04 12:49:26 -0400

    Add project files.

---
+++ Breadsticks/Models/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Breadsticks.Models
+{
+    public class Account
+    {
+        private decimal balance; 
+
+        protected Account(){}
+
+        protected decimal SetBalance(decimal newBalance)
+        {
+            balance = newBalance;
+            return balance;
+        }
+
+        protected decimal GetBalance()
+        {
+            return balance;
+        }
+
+        protected decimal AddToBalance(decimal amount)
+        {
+            balance += amount;
+            return balance;
+        }
+    }
+}
---
+++ Breadsticks/Models/CreditCard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Breadsticks.Models
+{
+    public class CreditCard : Account
+    {
+        private string name;
+        private string nickname;
+        private DateTime dueDate;
+
+        public CreditCard(string cardName)
+        {
+            SetName(cardName);
+            SetBalance(0.0m);
+        }
+
+        public CreditCard(string cardName, decimal initBalance)
+        {
+            SetName(cardName);
+            SetBalance(initBalance);
+        }
+
+        public string SetName(string newName)
+        {
+            name = newName;
+            return name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
